c60811d8c16ba97741711821154c11fd91c51e9e
Hazal Mestci <101270785+mestcihazal@users.noreply.github.com>
2023-07-10 11:07:07 -0400

    Update lightupbot.py

--- Light up bot/lightupbot.py
+++ Light up bot/lightupbot.py
@@ -5,7 +5,7 @@ from viam.robot.client import RobotClient
 from viam.rpc.dial import Credentials, DialOptions
 from viam.components.camera import Camera
 from viam.components.types import CameraMimeType
-from viam.services.vision import VisionServiceClient, VisModelConfig, VisModelType, Detection
+from viam.services.vision import VisionClient, VisModelConfig, VisModelType, Detection
 from PIL import ImageDraw
 from kasa import Discover, SmartPlug
 
@@ -23,53 +23,29 @@ async def connect():
     )
     return await RobotClient.at_address(robot_address, opts)
 
-#for displaying your detections with a red box around them, if needed
-def drawBox(d, frame):
-  rect = [d.x_min, d.y_min, d.x_max, d.y_max]
-  img1 = ImageDraw.Draw(frame)
-  img1.rectangle(rect, outline="red")
-  return frame
-
 async def main():
     robot = await connect()
 
-    print('Resources:')
-    print(robot.resource_names)
     # This string should match your camera component name in your robot config on the Viam app
     camera = Camera.from_robot(robot, "my-camera")
-    image = await camera.get_image()
-
-    # Get and set up the Vision Service
-    vision = VisionServiceClient.from_robot(robot)
-    params = {"model_path": "./effdet0.tflite", "label_path": "./labels.txt", "num_threads": 1}
-    personDet = VisModelConfig(name="person_detector", type=VisModelType("tflite_detector"), parameters=params)
-    await vision.add_detector(personDet)
-    names = await vision.get_detector_names()
-    print(names)
+    detector = VisionClient.from_robot(robot, "myPeopleDetector")
 
     N = 100
       
     #example: plug = SmartPlug('10.1.11.221')
     plug = SmartPlug('replace with the host IP of your plug')
     await plug.update()
-
     await plug.turn_off()
     state = "off"
     for i in range(N):
-        image = await camera.get_image()
-        detections = await vision.get_detections(image, "person_detector")
+        detections = await detector.get_detections_from_camera("my-camera")
         found = False
         for d in detections:
             if d.confidence > 0.8:
-                print(d)
-                print()
-                #you can use the below code for testing
-                #image = drawBox(d, image)
-                #image.show()
+                print(d.class_name)
                 if d.class_name.lower() == "person":
                     print("This is a person!")
                     found = True
-             
         if found:
             #turn on the smart plug
             await plug.turn_on()

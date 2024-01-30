f600ecf8134c5f4f95aefef86b7e634dc318df8a
Naomi Pentrel <5212232+npentrel@users.noreply.github.com>
2024-01-30 13:46:19 +0100

    Revert "Update tipsy.py to use sensors service (#16)"
    This reverts commit bc0798243bf5cade06587d88b739ca7b65d7e97b.

--- tipsy-bot/tipsy.py
+++ tipsy-bot/tipsy.py
@@ -6,7 +6,6 @@ from viam.rpc.dial import Credentials, DialOptions
 from viam.components.sensor import Sensor
 from viam.components.base import Base
 from viam.services.vision import VisionClient
-from viam.services.sensors import SensorsClient
 
 robot_api_key = os.getenv('ROBOT_API_KEY') or ''
 robot_api_key_id = os.getenv('ROBOT_API_KEY_ID') or ''
@@ -15,11 +14,8 @@ robot_address = os.getenv("ROBOT_ADDRESS") or ""
 base_name = os.getenv("ROBOT_BASE") or "tipsy-base"
 # change this if you named your camera differently in your robot configuration
 camera_name = os.getenv("ROBOT_CAMERA") or "cam"
-# change this if you named your detector differently in your robot configuration
-detector_name = os.getenv("ROBOT_DETECTOR") or "detect"
-# change this if you named your sensor service differently in your robot configuration
-sensor_service_name = (os.getenv("ROBOT_SENSORS") or "sensors")
-# change this if you named your sensor service differently in your robot configuration
+# change this if you named your sensors differently in your robor configuration
+sensor_names = (os.getenv("ROBOT_SENSORS") or "ultrasonic,ultrasonic2").split(",")
 pause_interval = os.getenv("PAUSE_INTERVAL") or 3
 
 if isinstance(pause_interval, str):
@@ -36,14 +32,18 @@ async def connect():
     return await RobotClient.at_address(robot_address, opts)
 
 
-async def get_obstacle_readings(sensors: list[Sensor], sensors_svc: SensorsClient):
-    print(await sensors_svc.get_readings(sensors))
-    return [r["distance"] for r in (await sensors_svc.get_readings(sensors)).values()]
+async def obstacle_detect(sensor: Sensor):
+    reading = (await sensor.get_readings())["distance"]
+    return reading
 
 
-async def obstacle_detect_loop(sensors: list[Sensor], sensors_svc: SensorsClient, base: Base):
+async def gather_obstacle_readings(sensors: list[Sensor]):
+    return await asyncio.gather(*[obstacle_detect(sensor) for sensor in sensors])
+
+
+async def obstacle_detect_loop(sensors: list[Sensor], base: Base):
     while True:
-        distances = await get_obstacle_readings(sensors, sensors_svc)
+        distances = await gather_obstacle_readings(sensors)
         if any(distance < 0.4 for distance in distances):
             # stop the base if moving straight
             if base_state == "straight":
@@ -52,7 +52,7 @@ async def obstacle_detect_loop(sensors: list[Sensor], sensors_svc: SensorsClient
         await asyncio.sleep(0.01)
 
 
-async def person_detect(detector: VisionClient, sensors: list[Sensor], sensors_svc: SensorsClient, base: Base):
+async def person_detect(detector: VisionClient, sensors: list[Sensor], base: Base):
     while True:
         # look for person
         found = False
@@ -67,7 +67,7 @@ async def person_detect(detector: VisionClient, sensors: list[Sensor], sensors_s
         if found:
             print("I see a person")
             # first manually call obstacle_detect - don't even start moving if someone is in the way
-            distances = await get_obstacle_readings(sensors, sensors_svc)
+            distances = await gather_obstacle_readings(sensors)
             if all(distance > 0.4 for distance in distances):
                 print("will move straight")
                 base_state = "straight"
@@ -85,14 +85,13 @@ async def person_detect(detector: VisionClient, sensors: list[Sensor], sensors_s
 async def main():
     robot = await connect()
     base = Base.from_robot(robot, base_name)
-    sensors_svc = SensorsClient.from_robot(robot, name=sensor_service_name)
-    sensors = await sensors_svc.get_sensors()
-    detector = VisionClient.from_robot(robot, name=detector_name)
+    sensors = [Sensor.from_robot(robot, sensor_name) for sensor_name in sensor_names]
+    detector = VisionClient.from_robot(robot, "myPeopleDetector")
 
     # create a background task that looks for obstacles and stops the base if its moving
-    obstacle_task = asyncio.create_task(obstacle_detect_loop(sensors, sensors_svc, base))
+    obstacle_task = asyncio.create_task(obstacle_detect_loop(sensors, base))
     # create a background task that looks for a person and moves towards them, or turns and keeps looking
-    person_task = asyncio.create_task(person_detect(detector, sensors, sensors_svc, base))
+    person_task = asyncio.create_task(person_detect(detector, sensors, base))
     results = await asyncio.gather(obstacle_task, person_task, return_exceptions=True)
     print(results)
 
